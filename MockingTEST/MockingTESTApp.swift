8ae7b6bb46eb0af65119794034acfb6d58949457
youcefhedibel <youyou_dz1@live.fr>
2023-03-14 11:47:50 +0100

    Initial Commit

---
+++ MockingTEST/MockingTESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockingTESTApp.swift
+//  MockingTEST
+//
+//  Created by mac on 14/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct MockingTESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
